c8c7b5ae90c8176831d57b5b1785c73fec50e181
Jânio <janioofi@gmail.com>
2023-11-23 19:53:41 -0300

    First Commit

---
+++ system_gym/src/main/java/br/janioofi/system_gym/SystemGymApplication.java
@@ -0,0 +1,13 @@
+package br.janioofi.system_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemGymApplication.class, args);
+    }
+
+}
---
+++ system_gym/src/test/java/br/janioofi/system_gym/SystemGymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.janioofi.system_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
